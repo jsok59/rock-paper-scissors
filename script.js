fae7d56111e9c1e32185a28009eafca428d38ee1
John Sok <98001557+jsok59@users.noreply.github.com>
2023-10-21 21:36:36 -0400

    finish adding elements and win condition

--- script.js
+++ script.js
@@ -39,16 +39,14 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-let game = function() {
-    for (let i = 0; i < 5; i++) {
-        computerSelection = getComputerChoice();
-        playerSelection = prompt("Enter either Rock, Paper, or Scissors").toLowerCase();
-        
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);                    //Change first letter of input to uppercase and all else lowercase
-        if (playerSelection != "Rock" && playerSelection != "Paper" && playerSelection != "Scissors") {
-            console.log("Invalid Input");
-        } 
 
+
+
+let game = function() {
+    //for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice();
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);                    //Change first letter of input to uppercase and all else lowercase
+     
         result = playRound(playerSelection, computerSelection);
         winOrLose = result.split(" ")[1];
         if (winOrLose ==="win!") {
@@ -56,26 +54,60 @@ let game = function() {
         } else if (winOrLose ==="lose!") {
             computerScore++;
         }
-        console.log(result);
-    }
+        divResult.textContent = result;
+        divScore.textContent = `Player Score : ${playerScore} | Computer Score: ${computerScore}`;
+        if (playerScore == 5) {
+            youLoseWin.textContent = 'You Win! Yay';
+            buttons.forEach((button) => {
+                button.remove();
+            })
+            container.appendChild(youLoseWin);
+        } else if (computerScore == 5) {
+            youLoseWin.textContent = 'You Lose! Boo';
+            buttons.forEach((button) => {
+                button.remove();
+            })
+            container.appendChild(youLoseWin);
+        }
+
 }
+
+
 
 let computerSelection;
 let playerSelection;
 let result;
 let playerScore = 0;
 let computerScore = 0;
+const youLoseWin = document.createElement('p')
+youLoseWin.setAttribute('style', 'color:red; font-size:50px;');
+const divScore = document.createElement('div');
+const divResult = document.createElement('div');
+// const divWinner = document.createElement('div');
+const container = document.querySelector('.container');
+const container2 = document.querySelector('.container2');
+container2.appendChild(divScore);
+container2.appendChild(divResult);
+// container2.appendChild(divWinner);
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) =>{
+    
+    button.addEventListener('click', ()=>{
+        playerSelection = button.className;
+       
+        game();
+    }) 
+
+})
 
 
-
-game();
-
-if (playerScore > computerScore) {
-    console.log("You win!");
-} else if(playerScore < computerScore) {
-    console.log("You lose!") 
-} else {
-    console.log("It's a tie!")
-}
+// if (playerScore > computerScore) {
+//     console.log("You win!");
+// } else if(playerScore < computerScore) {
+//     console.log("You lose!") 
+// } else {
+//     console.log("It's a tie!")
+// }
 
 
